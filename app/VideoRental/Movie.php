0588af6e93320f6b4d5ee79664378d8afb1196b9
oomusou <oomusou@gmail.com>
2016-01-09 16:35:37 +0800

    第一個測試：第二個紅燈

---
+++ app/VideoRental/Movie.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace VideoRental;
+
+
+class Movie
+{
+
+    /**
+     * Movie constructor.
+     * @param string $string
+     */
+    public function __construct($string)
+    {
+    }
+}
