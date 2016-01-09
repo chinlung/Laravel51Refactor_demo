7a58b7e2bfbee5b34e3e02d4f585596b132ba190
oomusou <oomusou@gmail.com>
2016-01-09 22:02:05 +0800

    第二個測試：第一個紅燈

--- tests/CustomerTest.php
+++ tests/CustomerTest.php
@@ -9,6 +9,10 @@ use VideoRental\Order;
 
 class CustomerTest extends TestCase
 {
+    /**
+     * @group Sam
+     * @group CustomerTest
+     */
     public function test_order_1_regular_movie_with_10_days()
     {
         /** arrange */
@@ -18,6 +22,27 @@ class CustomerTest extends TestCase
         $target->addOrder($order);
 
         $expected = 130;
+
+        /** act */
+        $actual = $target->calculateTotalPrice();
+
+        /** assert */
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * @group Sam
+     * @group CustomerTest
+     */
+    public function test_order_1_new_release_movie_with_5_days()
+    {
+        /** arrange */
+        $movie = new Movie('NewRelease');
+        $order = new Order($movie, 5);
+        $target = new Customer();
+        $target->addOrder($order);
+
+        $expected = 210;
 
         /** act */
         $actual = $target->calculateTotalPrice();
