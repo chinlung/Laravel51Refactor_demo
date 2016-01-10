86e838dc3e42ae07cdc645b37b71c76367a1cdff
oomusou <oomusou@gmail.com>
2016-01-10 12:33:56 +0800

    第二個測試：補齊Movie->getType()

--- app/VideoRental/Movie.php
+++ app/VideoRental/Movie.php
@@ -6,12 +6,25 @@ namespace VideoRental;
 
 class Movie
 {
+    /** @var string */
+    private $type;
 
     /**
      * Movie constructor.
-     * @param string $string
+     * @param string $type
      */
-    public function __construct($string)
+    public function __construct(string $type)
     {
+        $this->type = $type;
+    }
+
+    /**
+     * 回傳Movie type字串
+     *
+     * @return string
+     */
+    public function getType() : string
+    {
+        return $this->type;
     }
 }
