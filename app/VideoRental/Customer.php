05adad763fc534f81400880f8bbf98168e90bf66
oomusou <oomusou@gmail.com>
2016-01-10 21:28:27 +0800

    重構：Move Method Order->calculatePrice()

--- app/VideoRental/Customer.php
+++ app/VideoRental/Customer.php
@@ -35,37 +35,10 @@ class Customer
         $totalPrice = 0;
 
         foreach ($this->orders as $order) {
-            $totalPrice += $this->calculatePrice($order);
+            /** @var Order $order */
+            $totalPrice += $order->calculatePrice();
         }
 
         return $totalPrice;
-    }
-
-    /**
-     * 計算每筆訂單的金額
-     *
-     * @param Order $order
-     * @return int
-     */
-    public function calculatePrice(Order $order) : int
-    {
-        $price = 0;
-
-        /** @var Order $order */
-        switch ($order->getMovie()->getType()) {
-            case 'Regular':
-                $price += 100;
-                $price += ($order->getDays() - 7) * 10;
-                return $price;
-            case 'NewRelease':
-                $price += 150;
-                $price += ($order->getDays() - 3) * 30;
-                return $price;
-            case 'Children':
-                $price += 40;
-                $price += ($order->getDays() - 7) * 10;
-                return $price;
-        }
-        return $price;
     }
 }
--- app/VideoRental/Order.php
+++ app/VideoRental/Order.php
@@ -23,6 +23,32 @@ class Order
     }
 
     /**
+     * 計算每筆訂單的金額
+     *
+     * @return int
+     */
+    public function calculatePrice() : int
+    {
+        $price = 0;
+
+        switch ($this->getMovie()->getType()) {
+            case 'Regular':
+                $price += 100;
+                $price += ($this->getDays() - 7) * 10;
+                return $price;
+            case 'NewRelease':
+                $price += 150;
+                $price += ($this->getDays() - 3) * 30;
+                return $price;
+            case 'Children':
+                $price += 40;
+                $price += ($this->getDays() - 7) * 10;
+                return $price;
+        }
+        return $price;
+    }
+
+    /**
      * 回傳Movie物件
      *
      * @return Movie
